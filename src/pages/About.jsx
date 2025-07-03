963f8932d7489d8e23a62a2c0a1722737c34de07
Emma-Nuell <okoyemcpaul86@gmail.com>
2025-07-03 22:35:46 +0100

    about-page-git-fix

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,20 @@
+import React from "react";
+import {
+  Overview,
+  MissionVision,
+  Disciplines,
+  PresidentMessage,
+} from "../components/About";
+
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
-export default About
+    <div className='min-h-screen bg-white text-gray-800 space-y-24 md:px-6 py-12'>
+      <Overview />
+      <MissionVision />
+      <Disciplines />
+      <PresidentMessage />
+    </div>
+  );
+};
+
+export default About;
