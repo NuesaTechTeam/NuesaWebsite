b27ed155779248d8da21f25b033b3e70a14ea5d5
Emma-Nuell <okoyemcpaul86@gmail.com>
2025-11-07 10:33:07 +0100

    date change

--- src/lib/constants.jsx
+++ src/lib/constants.jsx
@@ -175,7 +175,7 @@ export const eventsData = [
   {
     id: 6,
     title: "Casablanca",
-    date: "November 29, 2025",
+    date: "November 22, 2025",
     time: "06:00 PM - 10:30 PM",
     venue: "Alfa Belgore Hall, ABUAD",
     description: "Annual dinner and awards night.",
